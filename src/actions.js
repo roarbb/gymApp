dec58509b3e3ce1cb16d84bf377d8f19932226a5
Matej Sajgal <matej@sajgal.com>
2016-08-31 23:33:30 +1000

    Quick thunk experiment

--- src/actions.js
+++ src/actions.js
@@ -1,14 +1,32 @@
 import fetch from 'isomorphic-fetch'
 
-export const saveLoggedUser = (response) => {
+const setFacebookUser = (response) => {
   return {
-    type: 'SAVE_LOGGED_USER',
+    type: 'SAVE_FACEBOOK_USER',
     response: response
   }
-};
+}
+
+const setApiUser = (response) => {
+  return {
+    type: 'SAVE_API_USER',
+    response: response
+  }
+}
+
+export const saveLoggedUser = (response) => {
+  return dispatch => {
+    dispatch(setFacebookUser(response));
+
+    return fetch(`http://localhost:8081/user/${response.email}`)
+      .then(response => response.json())
+      .then(json => dispatch(setApiUser(json)))
+      .catch(response => console.log(response))
+  }
+}
 
 export const logoutUser = () => {
   return {
     type: 'LOGOUT_USER'
   }
-};
+}
--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,11 +1,20 @@
 const user = (state = {}, action) => {
   switch (action.type) {
-    case 'SAVE_LOGGED_USER':
+    case 'SAVE_FACEBOOK_USER':
       if(!action.response.name) {
         return state;
       }
 
       return action.response
+
+    case 'SAVE_API_USER':
+      if(!action.response.hash) {
+        return state;
+      }
+
+      return Object.assign({}, state, {
+        hash: action.response.hash
+      });
 
     case 'LOGOUT_USER':
       return {};
