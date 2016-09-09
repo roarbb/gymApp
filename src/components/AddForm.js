0cdf4ef4712a89a460334ee96c19788d1cb80baa
Matej Sajgal <matej@sajgal.com>
2016-09-09 22:50:27 +1000

    * added form element

--- src/components/AddForm.js
+++ src/components/AddForm.js
@@ -1,5 +1,5 @@
 import React, {PropTypes} from 'react'
-import {Card, Row, Col, FormIconField, FormInput, Button} from 'elemental'
+import {Card, Row, Col, Form, FormIconField, FormInput, Button} from 'elemental'
 
 const AddForm = () => {
   return (
@@ -7,13 +7,15 @@ const AddForm = () => {
       <Card>
         <Row className="clearfix">
           <Col md="60%" sm="100%" className="m-x-auto">
-            <FormIconField iconPosition="left" iconKey="bookmark" iconFill="primary">
-              <FormInput placeholder="Discipline" size="lg" />
-            </FormIconField>
-            <FormIconField iconPosition="left" iconKey="database" iconFill="danger">
-              <FormInput placeholder="Weight" size="lg" />
-            </FormIconField>
-            <Button type="hollow-success" className="pull-xs-right">Save</Button>
+            <Form>
+              <FormIconField iconPosition="left" iconKey="bookmark" iconFill="primary">
+                <FormInput placeholder="Discipline" size="lg" />
+              </FormIconField>
+              <FormIconField iconPosition="left" iconKey="database" iconFill="danger">
+                <FormInput placeholder="Weight" size="lg" />
+              </FormIconField>
+              <Button submit type="hollow-success" className="pull-xs-right">Save</Button>
+            </Form>
           </Col>
         </Row>
       </Card>
