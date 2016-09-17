2766c48a79bcf647e8383aa5d57123188bdceeda
Matej Sajgal <matej@sajgal.com>
2016-09-17 20:48:47 +1000

    switch to react-create-app

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import React from 'react'
 import {render} from 'react-dom'
 import Root from './containers/Root'
 
-require('./styles/elemental.less');
-require('./styles/main.scss');
+import './styles/elemental.min.css'
+import './styles/main.css'
 
 render(
   <Root />,
